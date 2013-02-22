6a8d81391bce070aba8c5d4dde47795bac7cb2dd
boikoro <roman.boiko@ubs.com>
2013-02-22 18:38:10 +0000

    [tests] Acceptance tests refactored to use test setup for login aquiring

--- tests/acceptance_tests.py
+++ tests/acceptance_tests.py
@@ -6,7 +6,6 @@ from xml.dom.minidom import parseString
 import betfair_api
 import adapter_context
 import time
-from business_layer import SessionStorage
 
 HOST="localhost"
 PORT=int(adapter_context.BETFAIR_API_PORT)
@@ -15,116 +14,127 @@ BETFAIR_SERVICE = "/BFGlobalService"
 ERROR_CODE_TAG="errorCode"
 SESSION_TOKEN_TAG="sessionToken"
 
+SOAP_ENVELOPE = """<?xml version="1.0" encoding="UTF-8"?>
+                    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bfg="http://www.betfair.com/publicapi/v3/BFGlobalService/">
+                       <soapenv:Header/>
+                       <soapenv:Body>%s</soapenv:Body>
+                    </soapenv:Envelope>
+                """
 
-class SessionManagementAcceptanceTest(unittest.TestCase):
+loginRequestTemplate = """<bfg:login>
+                             <bfg:request>
+                                <ipAddress>0</ipAddress>
+                                <locationId>0</locationId>
+                                <password>%s</password>
+                                <productId>82</productId>
+                                <username>%s</username>
+                                <vendorSoftwareId>0</vendorSoftwareId>
+                             </bfg:request>
+                          </bfg:login>"""
+
+logoutRequestTemplate = """<bfg:logout>
+                             <bfg:request>
+                                <header>
+                                   <clientStamp>0</clientStamp>
+                                   <sessionToken>%s</sessionToken>
+                                </header>
+                             </bfg:request>
+                          </bfg:logout>"""
+
+getEventsRequestTemplate = """<bfg:getEvents>
+                                 <bfg:request>
+                                    <header>
+                                       <clientStamp>0</clientStamp>
+                                       <sessionToken>%s</sessionToken>
+                                    </header>
+                                    <eventParentId>%s</eventParentId>
+                                    <locale>en_UK</locale>
+                                 </bfg:request>
+                              </bfg:getEvents>"""
+
+getAllEventTypesRequestTemplate = """<bfg:getAllEventTypes>
+                                 <bfg:request>
+                                    <header>
+                                       <clientStamp>0</clientStamp>
+                                       <sessionToken>%s</sessionToken>
+                                    </header>
+                                    <locale>en_UK</locale>
+                                 </bfg:request>
+                              </bfg:getAllEventTypes>"""
+
+class AdapterAcceptanceTest(unittest.TestCase):
+    def assertErrorCodeInHeaderIs(self, response, expectedErrorCode):
+        self.assertEquals(textFromElement(response, ERROR_CODE_TAG, 0), expectedErrorCode)
+    def assertResultErrorCodeIs(self, response, expectedErrorCode):
+        self.assertEquals(textFromElement(response, ERROR_CODE_TAG, 1), expectedErrorCode)
+    def assertErrorCodesAreOk(self, responseDom):
+        self.assertErrorCodeInHeaderIs(responseDom, betfair_api.ERROR_CODE_OK)
+        self.assertResultErrorCodeIs(responseDom, betfair_api.ERROR_CODE_OK)
+
+class SessionManagementAcceptanceTest(AdapterAcceptanceTest):
 
     def test_that_valid_credentials_are_causing_successful_login(self):
-        responseDom = self.getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
+        responseDom = getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
         
         self.assertResultErrorCodeIs(responseDom, betfair_api.ERROR_CODE_OK)
-        sessionToken = self.sessionTokenFrom(responseDom)
-        self.assertEquals(len(sessionToken), SessionStorage.SESSION_TOKEN_LENGTH)
+        sessionToken = sessionTokenFrom(responseDom)
+        self.assertGreater(len(sessionToken), 0)
 
-        self.getLogoutResponseDom(sessionToken)
+        getLogoutResponseDom(sessionToken)
 
     def test_that_invalid_credentials_are_causing_login_failure(self):
-        responseDom = self.getLoginResponseDom('wrongLogin_' + str(time.time()), 'wrongPassword')
+        responseDom = getLoginResponseDom('wrongLogin_' + str(time.time()), 'wrongPassword')
         
         self.assertResultErrorCodeIs(responseDom, betfair_api.ERROR_INVALID_USERNAME_OR_PASSWORD)
         self.assertEquals(responseDom.getElementsByTagName(SESSION_TOKEN_TAG)[0].firstChild, None)
 
     def test_that_logout_with_nonexisting_session_token_results_unsuccessfuly(self):
-        responseDom = self.getLogoutResponseDom("invalidSessionToken")
+        responseDom = getLogoutResponseDom("invalidSessionToken")
         #THEN
         self.assertErrorCodeInHeaderIs(responseDom, betfair_api.ERROR_NO_SESSION)
         self.assertResultErrorCodeIs(responseDom, betfair_api.ERROR_API_ERROR)
 
     def test_that_logout_with_valid_session_token_results_successfuly(self):
-        loginResponseDom = self.getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
-        validSessionToken = self.sessionTokenFrom(loginResponseDom)
-        logoutResponseDom = self.getLogoutResponseDom(validSessionToken)
+        loginResponseDom = getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
+        validSessionToken = sessionTokenFrom(loginResponseDom)
+        logoutResponseDom = getLogoutResponseDom(validSessionToken)
         #THEN
-        self.assertErrorCodeInHeaderIs(logoutResponseDom, betfair_api.ERROR_CODE_OK)
-        self.assertResultErrorCodeIs(logoutResponseDom, betfair_api.ERROR_CODE_OK)
-        
-    def assertErrorCodeInHeaderIs(self, response, expectedErrorCode):
-        self.assertEquals(textFromElement(response, ERROR_CODE_TAG, 0), expectedErrorCode)
-    def assertResultErrorCodeIs(self, response, expectedErrorCode):
-        self.assertEquals(textFromElement(response, ERROR_CODE_TAG, 1), expectedErrorCode)
-    def sessionTokenFrom(self, response):
-        return textFromElement(response, SESSION_TOKEN_TAG, 0)
+        self.assertErrorCodesAreOk(logoutResponseDom)
 
-    loginRequest = """<bfg:login>
-                         <bfg:request>
-                            <ipAddress>0</ipAddress>
-                            <locationId>0</locationId>
-                            <password>%s</password>
-                            <productId>82</productId>
-                            <username>%s</username>
-                            <vendorSoftwareId>0</vendorSoftwareId>
-                         </bfg:request>
-                      </bfg:login>"""
 
-    def getLoginResponseDom(self, username, password):
-        loginRequest = soapMessage(self.loginRequest%(username, password))
-        return parseString(getServerReply(loginRequest))
+class EventsRetrievingTest(AdapterAcceptanceTest):
 
-    logoutRequest = """<bfg:logout>
-                         <bfg:request>
-                            <header>
-                               <clientStamp>0</clientStamp>
-                               <sessionToken>%s</sessionToken>
-                            </header>
-                         </bfg:request>
-                      </bfg:logout>"""
+    @classmethod
+    def setUpClass(cls):
+        loginResponseDom = getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
+        EventsRetrievingTest.validSessionToken = sessionTokenFrom(loginResponseDom)
 
-    def getLogoutResponseDom(self, sessionToken):
-        logoutRequest = soapMessage(self.logoutRequest%(sessionToken))
-        return parseString(getServerReply(logoutRequest))
-    
-    getAllEventTypesRequest = """<bfg:getAllEventTypes>
-         <bfg:request>
-            <header>
-               <clientStamp>0</clientStamp>
-               <sessionToken>%s</sessionToken>
-            </header>
-            <locale>en_UK</locale>
-         </bfg:request>
-      </bfg:getAllEventTypes>"""
+    @classmethod
+    def tearDownClass(cls):
+        getLogoutResponseDom(EventsRetrievingTest.validSessionToken)
 
     def test_that_fixed_dummy_list_of_event_types_is_returned(self):
-        request = soapMessage(self.getAllEventTypesRequest)
+        request = soapMessage(getAllEventTypesRequestTemplate%EventsRetrievingTest.validSessionToken)
         responseDom = parseString(getServerReply(request))
         self.assertEqual(textFromElement(responseDom, "name", 0), "Football")
         self.assertEqual(textFromElement(responseDom, "id", 0), "121005")
-        self.assertErrorCodeInHeaderIs(responseDom, betfair_api.ERROR_CODE_OK)
+        self.assertErrorCodesAreOk(responseDom)
     
-    getEventsRequest = """<bfg:getEvents>
-         <bfg:request>
-            <header>
-               <clientStamp>0</clientStamp>
-               <sessionToken>%s</sessionToken>
-            </header>
-            <eventParentId>%s</eventParentId>
-            <locale>en_UK</locale>
-         </bfg:request>
-      </bfg:getEvents>"""
       
     def test_that_list_of_football_parent_events_is_in_reponse_on_events_by_football_parentid(self):
-        loginResponseDom = self.getLoginResponseDom(adapter_context.TEST_SMK_PASSWORD, adapter_context.TEST_SMK_LOGIN)
-        validSessionToken = self.sessionTokenFrom(loginResponseDom)
         footballEventTypeId = "121005"
-        request = soapMessage(self.getEventsRequest%(validSessionToken, footballEventTypeId))
+        request = soapMessage(getEventsRequestTemplate%(EventsRetrievingTest.validSessionToken, footballEventTypeId))
         responseXml = getServerReply(request)
         responseDom = parseString(responseXml)
-
-        self.getLogoutResponseDom(validSessionToken)
         
         self.assertEqual(textFromElement(responseDom, "eventTypeId", 0), footballEventTypeId)
         self.assertEqual(textFromElement(responseDom, "eventParentId", 0), footballEventTypeId)
 
-        self.assertErrorCodeInHeaderIs(responseDom, betfair_api.ERROR_CODE_OK)
-        self.assertResultErrorCodeIs(responseDom, betfair_api.ERROR_CODE_OK)
+        self.assertErrorCodesAreOk(responseDom)
+
+
+
+
 
 def getServerReply(request):
     http_conn = httplib.HTTP(HOST, PORT)
@@ -139,16 +149,19 @@ def getServerReply(request):
     response = http_conn.getfile().read() 
     return response
 
+def getLogoutResponseDom(sessionToken):
+    logoutRequest = soapMessage(logoutRequestTemplate%(sessionToken))
+    return parseString(getServerReply(logoutRequest))
+
+def getLoginResponseDom(username, password):
+    loginRequest = soapMessage(loginRequestTemplate%(username, password))
+    return parseString(getServerReply(loginRequest))
 
 def textFromElement(dom, elementName, elementPosition):
     return dom.getElementsByTagName(elementName)[elementPosition].firstChild.nodeValue
 
-SOAP_ENVELOPE = """<?xml version="1.0" encoding="UTF-8"?>
-                    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bfg="http://www.betfair.com/publicapi/v3/BFGlobalService/">
-                       <soapenv:Header/>
-                       <soapenv:Body>%s</soapenv:Body>
-                    </soapenv:Envelope>
-                """
+def sessionTokenFrom(response):
+    return textFromElement(response, SESSION_TOKEN_TAG, 0)
 
 def soapMessage(body):
     return SOAP_ENVELOPE%(body)
