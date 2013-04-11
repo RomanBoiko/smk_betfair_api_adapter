aa566ea5ed92b97545d7da4c262a215d68ad36ca
boikoro <roman.boiko@ubs.com>
2013-04-11 21:06:51 +0100

    context properties moved from env vars to config

--- src/smkadapter/adapter_context.py
+++ src/smkadapter/adapter_context.py
@@ -2,23 +2,35 @@ import os
 import logging
 
 logging.basicConfig(level=logging.DEBUG)#, filename="server.log", filemode="w"
+from ConfigParser import SafeConfigParser
 
-TEST_SMK_LOGIN        = os.getenv("TEST_SMK_LOGIN")
-TEST_SMK_PASSWORD     = os.getenv("TEST_SMK_PASSWORD")
+class Config(object):
+    def __init__(self):
+        self.configParser = SafeConfigParser()
+        self.configParser.read('config/devenv.cfg')
 
-OAUTH_CONSUMER_KEY    = os.getenv("OAUTH_CONSUMER_KEY")
-OAUTH_CONSUMER_SECRET = os.getenv("OAUTH_CONSUMER_SECRET")
+    def defaultProperty(self, configKey):
+        return self.configParser.get('default', configKey)
 
-SMK_API_HOST          = os.getenv("SMK_API_HOST", "vagrant-dev.corp.smarkets.com")#api-sandbox.smarkets.com
-SMK_API_PORT          = os.getenv("SMK_API_PORT", "3700")
-SMK_REST_API_PORT     = os.getenv("SMK_REST_API_PORT", "8007")
-SMK_WEB_INTERFACE_PORT= os.getenv("SMK_WEB_INTERFACE_PORT", "8091")
+    def testProperty(self, configKey):
+        return self.configParser.get('test', configKey)
 
-SMK_WEB_URL = "http://%s:%s" % (SMK_API_HOST, SMK_WEB_INTERFACE_PORT)
-SMK_REST_API_URL = "http://%s:%s" % (SMK_API_HOST, SMK_REST_API_PORT)
-SMK_OAUTH_AUTHORIZE_URL = "%s/account/applications/authorize" % (SMK_WEB_URL)
+config = Config()
 
-BETFAIR_API_PORT      = os.getenv("BETFAIR_API_PORT", "8888")
+OAUTH_CONSUMER_KEY = config.defaultProperty('OAUTH_CONSUMER_KEY')
+OAUTH_CONSUMER_SECRET = config.defaultProperty('OAUTH_CONSUMER_SECRET')
+SMK_API_HOST = config.defaultProperty('SMK_API_HOST')
+SMK_API_PORT = config.defaultProperty('SMK_API_PORT')
+SMK_REST_API_PORT = config.defaultProperty('SMK_REST_API_PORT')
+SMK_WEB_INTERFACE_PORT = config.defaultProperty('SMK_WEB_INTERFACE_PORT')
+SMK_WEB_URL = config.defaultProperty('SMK_WEB_URL')
+SMK_REST_API_URL = config.defaultProperty('SMK_REST_API_URL')
+SMK_OAUTH_AUTHORIZE_URL = config.defaultProperty('SMK_OAUTH_AUTHORIZE_URL')
+BETFAIR_API_PORT = config.defaultProperty('BETFAIR_API_PORT')
+
+TEST_SMK_LOGIN = config.testProperty('TEST_SMK_LOGIN')
+TEST_SMK_PASSWORD = config.testProperty('TEST_SMK_PASSWORD')
+SMK_TEST_CONTEXT = config.testProperty('SMK_TEST_CONTEXT')
 
 def isAdapterRunningInTestContext():
-	return (os.getenv("SMK_TEST_CONTEXT") is not None)
+    return SMK_TEST_CONTEXT == "true"
--- tests/adapter_context_test.py
+++ tests/adapter_context_test.py
@@ -4,7 +4,21 @@ from smkadapter.adapter_context import *
 
 class AdapterContextTest(unittest.TestCase):
 
+    def assertPropertyWasSet(self, propertyValue):
+        self.assertNotEqual(propertyValue, None, "Config value not found in context")
+        self.assertNotEqual(propertyValue, "", "property is empty in config")
+
     def test_that_environment_was_set_up_for_testing(self):
-        variableNotSetMessage = "ENV Variable not set"
-        self.assertNotEqual(TEST_SMK_LOGIN, None, variableNotSetMessage)
-        self.assertNotEqual(TEST_SMK_PASSWORD, None, variableNotSetMessage)
+        self.assertPropertyWasSet(TEST_SMK_LOGIN)
+        self.assertPropertyWasSet(TEST_SMK_PASSWORD)
+        self.assertPropertyWasSet(OAUTH_CONSUMER_KEY)
+        self.assertPropertyWasSet(OAUTH_CONSUMER_SECRET)
+        self.assertPropertyWasSet(SMK_API_HOST)
+        self.assertPropertyWasSet(SMK_API_PORT)
+        self.assertPropertyWasSet(SMK_REST_API_PORT)
+        self.assertPropertyWasSet(SMK_WEB_INTERFACE_PORT)
+        self.assertPropertyWasSet(SMK_WEB_URL)
+        self.assertPropertyWasSet(SMK_REST_API_URL)
+        self.assertPropertyWasSet(SMK_OAUTH_AUTHORIZE_URL)
+        self.assertPropertyWasSet(BETFAIR_API_PORT)
+        self.assertPropertyWasSet(SMK_TEST_CONTEXT)
