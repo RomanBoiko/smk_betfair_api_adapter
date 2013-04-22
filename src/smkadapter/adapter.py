2c1c2f92b6fcd72d57b5314872c8557d91b5927e
boikoro <roman.boiko@ubs.com>
2013-04-22 05:09:02 +0100

    proof on concept in no-zsi: login implemented

--- src/smkadapter/adapter.py
+++ src/smkadapter/adapter.py
@@ -1,10 +1,16 @@
 import logging
+import threading
+import BaseHTTPServer
+import SimpleHTTPServer
+
 from ZSI.ServiceContainer import ServiceContainer, SOAPRequestHandler
 
 from betfair.BFGlobalService_server import BFGlobalService
 from betfair.BFExchangeService_server import BFExchangeService
 
+
 import adapter_context
+import betfair_api_nozsi
 
 LOGGER = logging.getLogger('[adapter]')
 
@@ -18,13 +24,39 @@ class WsdlSOAPRequestHandler(SOAPRequestHandler):
         wsdl = open(wsdlfile).read()
         self.send_xml(wsdl)
         
-def BetfairApiServer(port=80, services=(), RequestHandlerClass=SOAPRequestHandler):
+def start_zsi_server(port=80, services=(), RequestHandlerClass=SOAPRequestHandler):
     address = ('127.0.0.1', port)
     sc = ServiceContainer(address, RequestHandlerClass=RequestHandlerClass)
     for service in services:
         path = service.getPost()
         sc.setNode(service, path)
+    LOGGER.info("Starting SMK_BETFAIR adapter on port %s" % adapter_context.BETFAIR_API_PORT)
     sc.serve_forever()
-    
-LOGGER.info("Starting SMK_BETFAIR adapter on port %s" % adapter_context.BETFAIR_API_PORT)
-BetfairApiServer(port=int(adapter_context.BETFAIR_API_PORT), services=[BFGlobalService(), BFExchangeService()], RequestHandlerClass=WsdlSOAPRequestHandler)
+
+
+
+class NoZsiPostHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers.getheader('content-length'))
+        request_string = self.rfile.read(length)
+        print "====>"+request_string
+        # try:
+        result = betfair_api_nozsi.dispatchRequest(request_string)
+        # except:
+        #     result = 'error'
+        self.wfile.write(result)
+
+
+def start_no_zsi_server():
+    """Start the server."""
+    noZsiPort = 8889
+    server_address = ("127.0.0.1", noZsiPort)
+    server = BaseHTTPServer.HTTPServer(server_address, NoZsiPostHandler)
+    LOGGER.info("Starting SMK_BETFAIR adapter on port %s (noZsi)" % str(noZsiPort))
+    server.serve_forever()
+
+if __name__ == "__main__":
+    noZsiThread = threading.Thread(target=start_no_zsi_server)
+    noZsiThread.daemon = True
+    noZsiThread.start()
+    start_zsi_server(port=int(adapter_context.BETFAIR_API_PORT), services=[BFGlobalService(), BFExchangeService()], RequestHandlerClass=WsdlSOAPRequestHandler)
---
+++ src/smkadapter/betfair_api_nozsi.py
@@ -0,0 +1,50 @@
+from xmlrpclib import datetime
+import logging
+from lxml import etree
+import os
+import urllib2
+
+from jinja2 import Template
+
+from business_layer import BusinessUnit
+import smk_api
+
+ERROR_CODE_OK = "OK"
+ERROR_INVALID_USERNAME_OR_PASSWORD = "INVALID_USERNAME_OR_PASSWORD"
+ERROR_NO_SESSION = "NO_SESSION"
+ERROR_API_ERROR = "API_ERROR"
+ERROR_CANNOT_ACCEPT_BET = "CANNOT_ACCEPT_BET"
+ERROR_BET_NOT_CANCELLED = "BET_NOT_CANCELLED"
+ERROR_INVALID_MARKET = "INVALID_MARKET"
+
+BUSINESS_UNIT = BusinessUnit()
+LOGGER = logging.getLogger('[betfair.api]')
+
+def readFile(path):
+    fileStream = urllib2.urlopen("file://%s"%os.path.abspath(path))
+    data = fileStream.read()
+    fileStream.close()
+    return data
+
+def dispatchRequest(request):
+    requestType = etree.XML(request).xpath("local-name(//*[local-name()='Body']/*[1])")
+    if requestType=="login":
+        return login(request)
+    else:
+        LOGGER.error("reqyest type %s could not be dispatched"%requestType)
+        return "noAction"
+
+def login(request):
+    requestTree = etree.XML(request)
+    username = requestTree.xpath("//*[local-name()='username']/text()")[0]
+    password = requestTree.xpath("//*[local-name()='password']/text()")[0]
+    template = Template(readFile("templates/login.response.xml"))
+
+    loginResult = BUSINESS_UNIT.authenticateUserAndReturnHisSessionToken(username, password)
+    if loginResult.succeeded :
+        sessionToken = loginResult.result
+        accountStatus = BUSINESS_UNIT.getAccountFunds(sessionToken)
+        currency = accountStatus.currency
+        return template.render(sessionId=sessionToken, errorCode=ERROR_CODE_OK, currency=currency)
+    else:
+        return template.render(sessionId="", errorCode=ERROR_INVALID_USERNAME_OR_PASSWORD, currency="")
