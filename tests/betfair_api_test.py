208bceb400f0619c63568c0881343f0c147c7a68
boikoro <roman.boiko@ubs.com>
2013-04-29 21:06:56 +0100

    major actions tested with mocks. Everything works as expected so far

--- tests/betfair_api_test.py
+++ tests/betfair_api_test.py
@@ -44,14 +44,15 @@ def xmlElement(xml, elementName):
 
 def mockGetAccountFundsResponse(businessUnitMock):
     accountFundsResponseMock = businessUnitMock.getAccountFunds.return_value
-    accountFundsResponseMock.currency="GBP"    accountFundsResponseMock.cash=1111.00    accountFundsResponseMock.exposure=222.00
+    accountFundsResponseMock.currency="GBP"    accountFundsResponseMock.cash=1111.0    accountFundsResponseMock.exposure=222.0
     
 def mockSuccessfulLoginResponse(businessUnitMock):
     businessUnitMock.authenticateUserAndReturnHisSessionToken.return_value = smk_api.ActionSucceeded(SESSION_TOKEN)
+    
 
 class BetfairApiUnitTest(unittest.TestCase):
     @patch('smkadapter.betfair_api_nozsi.businessUnit')
-    def test_login_action_to_return_session_token(self, BusinessUnitMock):
+    def test_login_action_to_be_dispatched_and_to_return_session_token(self, BusinessUnitMock):
         businessUnitMock = BusinessUnitMock.return_value
         mockSuccessfulLoginResponse(businessUnitMock)
         mockGetAccountFundsResponse(businessUnitMock)
@@ -74,35 +75,64 @@ class BetfairApiIntegrationTest(unittest.TestCase):
         LOG.info("sleeping to allow web-server to stop...")
         time.sleep(1)
         LOG.info("starting test")
-        run_business_flow()
-
-
-def run_business_flow():
-    bf = bfclient.BfClient()
-
-    try:
-        bfrespLogin = bf.login(username=adapter_context.TEST_SMK_LOGIN, password=adapter_context.TEST_SMK_PASSWORD)
-        print bfrespLogin
-
-        accountFundsResponse = bf.getAccountFunds(bfpy.ExchangeUK)
-        print accountFundsResponse
-
-        # bfrespEventTypes = bf.getAllEventTypes()
-        # print bfrespEventTypes
-
-        # currentBets = bf.getCurrentBets(bfpy.ExchangeUK)
-        # print currentBets
         
-        # self.placeBets(bf)
+        bfClient = self.should_login_and_return_bfpy_BfClient(businessUnitMock)
+        self.should_access_account_funds(bfClient, businessUnitMock)
+        self.should_access_event_types(bfClient, businessUnitMock)
+        self.should_access_current_accounts_bets(bfClient, businessUnitMock)
+        self.should_logout_from_session(bfClient, businessUnitMock)
+        
+        
+    def should_login_and_return_bfpy_BfClient(self, businessUnitMock):
+        bfClient = bfclient.BfClient()
+        adapterResponse = bfClient.login(username=adapter_context.TEST_SMK_LOGIN, password=adapter_context.TEST_SMK_PASSWORD)
+        LOG.debug(adapterResponse)
+        businessUnitMock.authenticateUserAndReturnHisSessionToken.assert_called_with(adapter_context.TEST_SMK_LOGIN, adapter_context.TEST_SMK_PASSWORD)
+        self.assertTrue('sessionToken = "%s"'%SESSION_TOKEN in str(adapterResponse))
+        return bfClient
+    
+    def should_access_account_funds(self, bfClient, businessUnitMock):
+        adapterResponse = str(bfClient.getAccountFunds(bfpy.ExchangeUK))
+        LOG.debug(adapterResponse)
+        self.assertTrue('balance = 1111.0' in adapterResponse)
+        self.assertTrue('exposure = 222.0' in adapterResponse)
+        
 
-        bfLogout = bf.logout()
-        print bfLogout
-    except BfError, e:
-        print "adapter error: %s" % str(e)
-        raise
+    def should_access_event_types(self, bfClient, businessUnitMock):
+        adapterResponse = str(bfClient.getAllEventTypes())
+        LOG.debug(adapterResponse)
+        self.assertTrue('id = 125062' in adapterResponse)
+        self.assertTrue('name = "Football"' in adapterResponse)
+        
+    def should_access_current_accounts_bets(self, bfClient, businessUnitMock):
+        self.mockGetBetsForAccount(businessUnitMock)
+        adapterResponse = str(bfClient.getCurrentBets(bfpy.ExchangeUK))
+        LOG.debug(adapterResponse)
+        businessUnitMock.getBetsForAccount.assert_called()
+        self.assertTrue('betId = 123' in adapterResponse)
+        self.assertTrue('betId = 124' in adapterResponse)
+        
+    def mockGetBetsForAccount(self, businessUnitMock):
+        response = businessUnitMock.getBetsForAccount.return_value
+        orderId=123
+        marketId=234
+        contractId=333
+        price=400.0
+        status=1
+        quantity=244.0
+        createdDateInMillis="somedate"
+        response.bets=[]
+        response.bets.append(smk_api.BetDetails(orderId, marketId, contractId, price, status, quantity, createdDateInMillis, True))
+        response.bets.append(smk_api.BetDetails(orderId+1, marketId, contractId, price, status, quantity, createdDateInMillis, False))
+        
+    def should_logout_from_session(self, bfClient, businessUnitMock):
+        adapterResponse = str(bfClient.logout())
+        LOG.debug(adapterResponse)
+        businessUnitMock.logUserOutAndReturnResultOfAction.assert_called_with(SESSION_TOKEN)
+        self.assertTrue('errorCode = "OK"' in adapterResponse)
 
-def placeBets(bf):
-    placeBet = bf.createPlaceBets()
+def placeBets(bfClient):
+    placeBet = bfClient.createPlaceBets()
 
     placeBet.asianLineId = 0
     # Man City = 47999
@@ -117,12 +147,8 @@ def placeBets(bf):
     # English Premier League Winner 2011/2012
     placeBet.marketId = 135615
     
-    response = bf.placeBets(bfpy.ExchangeUK, bets=[placeBet])
-    print response
+    response = bfClient.placeBets(bfpy.ExchangeUK, bets=[placeBet])
 #        print 'sleeping 5 seconds'
 #        bet = response.betResults[0]
-
-#class AdapterExternalAcceptanceTest(unittest.TestCase):
-#    def test_adapter_flow(self):
-#        run_business_flow()
+    return response
 
